d2f5ebd0e90fc11246ef0aa535a84b948d2a9bf5
Jasleen Kaur <99jasleenkaur@gmail.com>
2018-03-31 19:37:53 -0700

    Create KeyNotFoundException.java

---
+++ Program-3/KeyNotFoundException.java
@@ -0,0 +1,10 @@
+// CMPS 12B
+// 2/7/2018
+// KeyNotFoundException.java
+
+public class KeyNotFoundException extends RuntimeException{
+	public KeyNotFoundException(String message){
+		super(message);
+	}
+}
+
