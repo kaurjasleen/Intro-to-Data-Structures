06cccc5f6dbea2d5e58d7b3b21064c95f66c58a4
Jasleen Kaur <99jasleenkaur@gmail.com>
2018-03-31 19:35:54 -0700

    Create DuplicateKeyException.java

---
+++ Program-3/DuplicateKeyException.java
@@ -0,0 +1,9 @@
+// CMPS 12B
+// 2/7/2018
+// DuplicateKeyException.java
+
+public class DuplicateKeyException extends RuntimeException{
+	public DuplicateKeyException(String message){
+		super(message);
+	}
+}
